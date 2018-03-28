c9bdc297820589aaad681c65e20034f879f9e546
Sameer Khan <sameer_khan@student.uml.edu>
2018-03-28 12:38:52 -0400

    Types and args

--- express-graphql/server.js
+++ express-graphql/server.js
@@ -2,17 +2,27 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 
-// construct a schema using graphql schema language
+const PORT = 4000;
+// construct a schema using graphql schema language (using different data types)
 const schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollDice(numDice: Int!, numSides: Int): [Int] 
   }
 `);
 
 // the root provides a resolver function for each API endpoint
-const root = {
-  hello: () => {
-    return 'Hello world!';
+const rootValue = {
+  quoteOfTheDay: () =>
+    Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within',
+  random: () => Math.random(),
+  rollDice: function({ numDice, numSides }) {
+    let output = [];
+    for (let i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
   }
 };
 
@@ -22,12 +32,12 @@ const app = express();
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: schema,
-    rootValue: root,
+    schema,
+    rootValue,
     graphiql: true
   })
 );
 
 // start the server at port 4000
-app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+app.listen(PORT);
+console.log(`Running a GraphQL API server at localhost:${PORT}/graphql`);
