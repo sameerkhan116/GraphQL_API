56909d99dc1ab1986e03db37389770a4c37bb08f
Sameer Khan <sameer_khan@student.uml.edu>
2018-03-28 22:06:11 -0400

    Mutations done

--- express-graphql/server.js
+++ express-graphql/server.js
@@ -4,26 +4,67 @@ const { buildSchema } = require('graphql');
 
 // construct a schema using graphql schema language
 const schema = buildSchema(`
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
   type Query {
-    hello: String
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
 `);
 
-// the root provides a resolver function for each API endpoint
-const root = {
-  hello: () => {
-    return 'Hello world!';
+class Message {
+  constructor(id, { content, author }) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+let fakeDatabase = {};
+
+const rootValue = {
+  getMessage: ({ id }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error(`no message with ${id} id exists`);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: ({ input }) => {
+    let id = require('crypto')
+      .randomBytes(10)
+      .toString('hex');
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: ({ id, input }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error(`no message with ${id} exists`);
+    }
+    fakeDatabase[id] = input;
+    return new Message(id, input);
   }
 };
-
 // setup express server with schema, rootvalue.
 // if you want graphiql, set it as true
 const app = express();
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: schema,
-    rootValue: root,
+    schema,
+    rootValue,
     graphiql: true
   })
 );
